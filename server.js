fbca012b8e0b175effca02b569421a3684199fbd
clarkdkyle <clarkdkyle@gmail.com>
2020-04-18 23:25:51 -0500

    l

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,15 +10,15 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
-// routes here
-app.use(require("./routes/api.js"));
+// routes
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
